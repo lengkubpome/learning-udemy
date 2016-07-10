0b89614303ff71e9afc95ada5889868f9ef2188a
Kiattiphong Saetang <lengkubpome@gmail.com>
2016-07-11 01:38:46 +0700

    Chapter 73

--- app/Form/signup-form.component.ts
+++ app/Form/signup-form.component.ts
@@ -1,15 +1,28 @@
 import { Component} from 'angular2/core';
-import { ControlGroup, Control, Validators } from 'angular2/common';
+import { ControlGroup, Control, Validators, FormBuilder } from 'angular2/common';
+import { UsernameValidators } from './usernameValidators';
 
 @Component({
     selector: 'signup-form',
     templateUrl: 'app/form/signup-form.component.html'
 })
 export class SignUpFormComponent {
-    form = new ControlGroup({
-        username: new Control('',Validators.required),
-        password: new Control('',Validators.required)
-    });
+    form: ControlGroup;
+
+    constructor(fb: FormBuilder){
+        this.form = fb.group({
+            username:['', Validators.compose([
+                Validators.required,
+                UsernameValidators.cannotContainSpace
+            ])],
+            password:['',Validators.required]
+        });
+    }
+
+    // form = new ControlGroup({
+    //     username: new Control('',Validators.required),
+    //     password: new Control('',Validators.required)
+    // });
 
     signup(){
         console.log(this.form.value); 
